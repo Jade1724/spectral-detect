49797f88626356e3f6e2136b118829bee8c2abc7
Haruka Ichinose <haruka.i1724@gmail.com>
2022-09-21 13:07:12 +1200

    Migrated project from GitLab to GitHub

---
+++ backend/src/test/java/com/example/weedbuster/WeedbusterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weedbuster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeedbusterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
